49b1bebdfea3673842cdcc6f2dbd0fefcdd9d5cf
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2025-03-20 18:00:40 +0200

    final

--- course_information/src/App.jsx
+++ course_information/src/App.jsx
@@ -1,43 +1,5 @@
-const Header = (props) => <h1>{props.course}</h1>
+import Course from './components/Course'
 
-const Content = (props) => {
-    console.log('homo',props.parts.map(part =>
-        <Part key={part.id} part={props.parts[part]}/>))
-    return (
-    <div>
-        {props.parts.map(part =>
-            <Part key={part.id} part={part}/>)}
-        {/*
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
-        */}
-    </div>
-    )
-}
-
-const Part = (props) => {
-    console.log('vittu', props.part)
-    return (
-        <p>
-            {props.part.name} {props.part.exercises}
-        </p>
-    )
-}
-
-const Total = (props) => <b>Total of {props.total} exercises</b>
-
-const Course = (props) => {
-    return (
-        <div>
-            <Header course={props.course.name}/>
-            <Content parts={props.course.parts}/>
-            <Total total={props.course.parts.reduce((accumulator,currentValue) => accumulator +
-                currentValue.exercises, 0)}/>
-        </div>
-    )
-
-}
 const App = () => {
     const courses = [
         {
