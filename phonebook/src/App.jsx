08cc10ad101946224e16eb19a348322ec42a2ea2
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2025-03-21 13:49:50 +0200

    phonebook modularisation

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
-
+import Filter from "./components/Filter.jsx";
+import PersonForm from "./components/PersonForm.jsx";
+import Persons from "./components/Persons.jsx";
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number:'1335919519819' },
-        { name: 'Nuutti Turunen', number:'2994028527892'}
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
@@ -44,35 +48,21 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <input
-                placeholder='filter shown with:'
-                value={filter}
+            <Filter
+                filter={filter}
                 onChange={changeFilter}
             />
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input
-                    value={newName}
-                    onChange={setName}
-                />
-                </div>
-                <div>
-                    number:<input
-                    value={newNumber}
-                    onChange={changeNumber}
-                    />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+            <PersonForm
+                onSubmit={addPerson}
+                newName={newName}
+                setName={setName}
+                newNumber={newNumber}
+                changeNumber={changeNumber}
+            />
             <h2>Numbers</h2>
-            <div>
-                <ul>
-                    {personsToShow.map(person => <li key={person.name}>
-                        {person.name} {person.number}</li>)}
-                </ul>
-            </div>
+            <Persons
+                personsToShow={personsToShow}
+            />
         </div>
     )
 }
