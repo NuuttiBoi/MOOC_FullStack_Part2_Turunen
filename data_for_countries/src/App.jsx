9c56196dd0b23db0b3f4a254f340ab0c1f8b5412
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2025-03-24 23:15:52 +0200

    api key

--- data_for_countries/src/App.jsx
+++ data_for_countries/src/App.jsx
@@ -6,7 +6,10 @@ import countryService from './services/countries'
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  const [filter, setFilter] = useState('')
+  const [filter, setFilter] = useState('');
+  const [temperature, setTemperature] = useState('');
+  const [icon, setIcon] = useState(null);
+  const [wind, setWind] = useState('');
 
     useEffect(() => {
         countryService.getAll()
@@ -22,14 +25,39 @@ const App = () => {
         setFilter(event.target.value)
     }
 
+    const getTemperature = (lat, lon) => {
+        countryService.getWeather(lat, lon)
+            .then(weather => setTemperature(weather.main.temp))
+    }
+
+    const Weather = (props) => {
+        countryService.getWeather(props.lat, props.lon)
+            //.then(weather => setTemperature(weather.main.temp))
+            .then(weather => {
+                setTemperature(weather.main.temp)
+                setIcon(`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+                setWind(weather.wind.speed)
+            })
+            //.then(weather => setIcon(`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`))
+        return (
+            <div>
+                <p>Temperature {temperature}</p>
+                <img src={icon} alt='weather icon'/>
+                <p>Wind {wind}</p>
+            </div>
+        )
+    }
+
     const FilteredCountries =  () => {
-        if(countriesToShow.length < 10 && countriesToShow.length > 2){
+        if(countriesToShow.length < 10 && countriesToShow.length > 1){
             console.log(countriesToShow.length)
             return (
                 <div>
-                    <ul>
+                    <ul style={{listStyleType: 'none'}}>
                         {countriesToShow.map(country =>
-                            <li key={country.name.common}>{country.name.common}</li>
+                            <li key={country.name.common}>{country.name.common} <button onClick={() =>
+                                setFilter(country.name.common)}>show</button>
+                            </li>
                         )}
                     </ul>
                 </div>
@@ -42,17 +70,24 @@ const App = () => {
             )
         } else if(countriesToShow.length === 1){
             const country = countriesToShow[0]
+            getTemperature(country.latlng[0],country.latlng[1])
             return (
                 <div>
                     <h1>{country.name.common}</h1>
                     <p>Capital {country.capital[0]}</p>
+                    <p>Area {country.area}</p>
                     <h2>Languages</h2>
-                    <ul>
+                    <ul style={{listStyleType: 'none'}}>
                         {Object.entries(country.languages).map(([key, value]) =>
-                        <li key={key}>{value} <button>show</button></li>
+                        <li key={key}>{value}</li>
                         )}
                     </ul>
                     <img src={country.flags.png} alt='country flag'/>
+                    <h2>Weather in {country.capital[0]}</h2>
+                    <Weather
+                        lat={country.latlng[0]}
+                        lon={country.latlng[1]}
+                    />
                 </div>
             )
         }
@@ -70,13 +105,6 @@ const App = () => {
 
     console.log('found countries',countriesToShow, countriesToShow.length)
 
-    const Countries = (props) => {
-        return (
-            <div>
-
-            </div>
-        )
-    }
 
   return (
       <div>
--- data_for_countries/src/services/countries.js
+++ data_for_countries/src/services/countries.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+const api_key = import.meta.env.VITE_API_KEY
 
 const getAll = () => {
     const request = axios.get(`${baseUrl}/all`);
@@ -10,4 +11,13 @@ const getCountry = (props) => {
     const request = axios.get(`${baseUrl}/name/${props.name.common.toLowerCase()}`);
     return request.then(response => response.data);
 }
-export default {getAll, getCountry}
+
+const getWeather = (lat, lon) => {
+    console.log(api_key)
+    const request = axios.
+    get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`);
+    return request.then(response => response.data);
+}
+
+
+export default {getAll, getCountry, getWeather}
