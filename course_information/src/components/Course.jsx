72e4783e6fa46da0f8b2f35295fb2ca67e37b099
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2025-03-25 01:52:26 +0200

    refactoring

--- course_information/src/components/Course.jsx
+++ course_information/src/components/Course.jsx
@@ -7,17 +7,11 @@ const Content = (props) => {
         <div>
             {props.parts.map(part =>
                 <Part key={part.id} part={part}/>)}
-            {/*
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
-        */}
         </div>
     )
 }
 
 const Part = (props) => {
-    console.log('vittu', props.part)
     return (
         <p>
             {props.part.name} {props.part.exercises}
